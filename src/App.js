88d5321cea34ffc39f0207da486c37baef16d618
orenansouza <renansouza.21.109@icloud.com>
2020-10-30 12:15:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  async function getRepos() {
+    if (user) {
+      const result = await axios.get(`https://api.github.com/users/${user}/repos`);
+      const repos = result.data.map(repo => ({
+        nome: repo.name,
+        criador: repo.owner.login,
+        "data criação": repo.created_at,
+        estrelas: repo.stargazers_count
+      }))
+      setRepos(repos)
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="containerInput">
+          <input type="text" placeholder="Informe seu usuário" onChange={e => setUser(e.target.value)} />
+          <div onClick={getRepos}>Buscar</div>
+        </div>
+        <div className="ag-theme-alpine" style={{ height: 400, width: 820 }}>
+          <AgGridReact rowData={repos}>
+            <AgGridColumn field="nome"></AgGridColumn>
+            <AgGridColumn field="criador"></AgGridColumn>
+            <AgGridColumn field="data criação"></AgGridColumn>
+            <AgGridColumn field="estrelas"></AgGridColumn>
+          </AgGridReact>
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
